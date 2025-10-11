47f3ab83f7b7c69217d783b94f6c73bcd208b440
mateo-morales-garcia-gamecoderstudios <mateo.morales@gamecoderstudios.com>
2025-10-10 22:19:43 -0500

    Add logout button

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,25 +1,41 @@
+import { useAuth } from '@/lib/auth';
 import { Link } from '@tanstack/react-router';
-import { useState } from 'react';
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className="p-1 flex items-center justify-between flex-row bg-gray-800 text-white shadow-lg">
       <h1 className="text-xl font-semibold">
         Rewards
       </h1>
-      <button
-        onClick={() => setIsOpen(true)}
-        className="p-2 hover:bg-gray-700 rounded-lg transition-colors"
-        aria-label="Login"
-      >
-        <Link to="/login">
-          <h1 className="text-xs font-semibold">
-            Login
-          </h1>
-        </Link>
-      </button>
+      <LoginLogoutButton />
     </header>
   );
 }
+
+function LoginLogoutButton() {
+  const { isAuthenticated, logout } = useAuth()
+
+  if (isAuthenticated) {
+    return <button
+      onClick={logout}
+      className="p-2 hover:bg-gray-700 rounded-lg transition-colors"
+      aria-label="Logout"
+    >
+      <h1 className="text-xs font-semibold">
+        Logout
+      </h1>
+    </button>
+  }
+  return <button
+    className="p-2 hover:bg-gray-700 rounded-lg transition-colors"
+    aria-label="Login"
+  >
+    <Link to="/login">
+      <h1 className="text-xs font-semibold">
+        Login
+      </h1>
+    </Link>
+  </button>
+}
+
